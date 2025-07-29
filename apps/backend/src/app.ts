64d4fab0f8d764bbb284eb52196ffcb729840a22
coder7475 <robiulhossain7475@gmail.com>
2025-07-29 22:48:55 +0600

    chore: update build script to copy OpenAPI spec to dist and remove Swagger UI integration

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -1,18 +1,13 @@
-import { readFileSync } from "node:fs";
 import indexRouter from "@/routes";
 import { mongoConnector } from "@repo/db";
 import cors from "cors";
 import express, { type Express } from "express";
 import helmet from "helmet";
-import swaggerUi from "swagger-ui-express";
 
 import { env } from "./configs/envConfig";
 import { middlewares } from "./middlewares";
 
 const app: Express = express();
-
-// Load OpenAPI spec
-const openapiSpec = JSON.parse(readFileSync("./openapi.json", "utf8"));
 
 // Connect to Database
 mongoConnector(env.DB_URI as string).catch((err: Error) => console.error(err));
@@ -22,10 +17,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
-
-// Routes using indexRouter
-// Serve Swagger UI at /api-docs
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(openapiSpec));
 
 app.use("/api/v1", indexRouter);
 app.use("/", (_req, res) => {
