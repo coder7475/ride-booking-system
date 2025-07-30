073b8c463a1447c086f2f30f8fb351200b5f320e
coder7475 <robiulhossain7475@gmail.com>
2025-07-30 13:13:37 +0600

    feat: refactor user creation logic to use async error handling and enhance user service with email uniqueness check and password hashing

--- apps/backend/src/modules/user/user.controller.ts
+++ apps/backend/src/modules/user/user.controller.ts
@@ -1,20 +1,17 @@
+import { catchAsync } from "@/utils/asyncHandler";
 import { NextFunction, Request, Response } from "express";
 
 import { userService } from "./user.service";
 
-const createUser = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const userData = req.body;
-    const user = await userService.createUser(userData);
-    res.status(201).json({
-      success: true,
-      message: "User created successfully",
-      data: user,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
+const createUser = catchAsync(async (req: Request, res: Response) => {
+  const userData = req.body;
+  const user = await userService.createUser(userData);
+  res.status(201).json({
+    success: true,
+    message: "User created successfully",
+    data: user,
+  });
+});
 
 export const userController = {
   createUser,
--- apps/backend/src/modules/user/user.service.ts
+++ apps/backend/src/modules/user/user.service.ts
@@ -1,8 +1,33 @@
+import AppError from "@/configs/AppError";
+import { AuthProviderNames, IAuthProvider } from "@/types/types";
+import { hashPassword } from "@repo/utils";
+
 import { IUser } from "./user.interface";
 import { UserModel } from "./user.model";
 
-const createUser = async (userData: IUser): Promise<any> => {
-  return await UserModel.create(userData);
+const createUser = async (userData: Partial<IUser>): Promise<any> => {
+  const { email, password, ...data } = userData;
+  const isUser = await UserModel.findOne({ email });
+
+  if (isUser) {
+    throw new AppError(400, "User Already Exist");
+  }
+
+  // hash the password before creating user
+  const hashedPassword = hashPassword(password!, 12);
+  const authProvider: IAuthProvider = {
+    provider: AuthProviderNames.LOCAL,
+    providerId: email as string,
+  };
+
+  const user = await UserModel.create({
+    email,
+    password: hashedPassword,
+    auths: [authProvider],
+    ...data,
+  });
+
+  return user;
 };
 
 export const userService = {
