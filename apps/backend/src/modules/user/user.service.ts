ceea7ec2bfd5222dfd01de48ab4b2b0a84048b32
coder7475 <robiulhossain7475@gmail.com>
2025-07-30 12:46:49 +0600

    refactor: rename User model to UserModel in user service for consistency

--- apps/backend/src/modules/user/user.service.ts
+++ apps/backend/src/modules/user/user.service.ts
@@ -1,8 +1,8 @@
 import { IUser } from "./user.interface";
-import { User } from "./user.model";
+import { UserModel } from "./user.model";
 
 const createUser = async (userData: IUser): Promise<any> => {
-  return await User.create(userData);
+  return await UserModel.create(userData);
 };
 
 export const userService = {
