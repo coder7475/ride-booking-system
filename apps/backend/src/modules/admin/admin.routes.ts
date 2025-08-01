9b89b8c4bf66618bfc5ce426ef130d5f70b1601e
coder7475 <robiulhossain7475@gmail.com>
2025-08-01 10:47:42 +0600

    feat: integrate admin controller for driver approval and rejection routes

--- apps/backend/src/modules/admin/admin.routes.ts
+++ apps/backend/src/modules/admin/admin.routes.ts
@@ -2,10 +2,21 @@ import { checkAuth } from "@/middlewares/CheckAuth";
 import { Role } from "@/types/types";
 import { Router } from "express";
 
+import { AdminController } from "./admin.controller";
+
 const adminRoutes: Router = Router();
 
 // approve or reject driver application
-adminRoutes.patch("/drivers/:id/approve", checkAuth(Role.ADMIN));
-adminRoutes.patch("/drivers/:id/reject", checkAuth(Role.ADMIN));
+adminRoutes.patch(
+  "/drivers/:id/approve",
+  checkAuth(Role.ADMIN),
+  AdminController.approveDriver,
+);
+
+adminRoutes.patch(
+  "/drivers/:id/reject",
+  checkAuth(Role.ADMIN),
+  AdminController.rejectDriver,
+);
 
 export default adminRoutes;
