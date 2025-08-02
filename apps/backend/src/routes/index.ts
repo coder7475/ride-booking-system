dc2c976671f850dcc4ccb052e8a19efddaf8f06a
coder7475 <robiulhossain7475@gmail.com>
2025-08-02 11:54:52 +0600

    refactor: standardize code formatting and indentation across route and type definition files for improved readability

--- apps/backend/src/routes/index.ts
+++ apps/backend/src/routes/index.ts
@@ -1,37 +1,37 @@
+import { Router } from "express";
 import adminRoutes from "@/modules/admin/admin.routes";
 import authRoutes from "@/modules/auth/auth.routes";
 import driverRoutes from "@/modules/driver/driver.routes";
 import RidesRouter from "@/modules/ride/ride.routes";
 import userRoutes from "@/modules/user/user.routes";
-import { Router } from "express";
 
 const indexRouter: Router = Router();
 
 const moduleRoutes = [
-  {
-    path: "/auth",
-    routes: authRoutes,
-  },
-  {
-    path: "/user",
-    routes: userRoutes,
-  },
-  {
-    path: "/drivers",
-    routes: driverRoutes,
-  },
-  {
-    path: "/rides",
-    routes: RidesRouter,
-  },
-  {
-    path: "/admin",
-    routes: adminRoutes,
-  },
+	{
+		path: "/auth",
+		routes: authRoutes,
+	},
+	{
+		path: "/user",
+		routes: userRoutes,
+	},
+	{
+		path: "/drivers",
+		routes: driverRoutes,
+	},
+	{
+		path: "/rides",
+		routes: RidesRouter,
+	},
+	{
+		path: "/admin",
+		routes: adminRoutes,
+	},
 ];
 
 moduleRoutes.forEach((route) => {
-  indexRouter.use(route.path, route.routes);
+	indexRouter.use(route.path, route.routes);
 });
 
 export default indexRouter;
