1c4ea8a9f8629befa9245d725db0f1a7028538e7
coder7475 <robiulhossain7475@gmail.com>
2025-08-29 10:37:06 +0600

    refactor: folder structure

--- apps/frontend/src/pages/User/Assessment.tsx
+++
@@ -1,9 +0,0 @@
-const Assessment = () => {
-  return (
-    <div>
-      English Assessment page
-    </div>
-  );
-};
-
-export default Assessment;
---
+++ apps/frontend/src/pages/User/Bookings.tsx
@@ -0,0 +1,5 @@
+const Bookings = () => {
+  return <div>Bookings page</div>;
+};
+
+export default Bookings;
