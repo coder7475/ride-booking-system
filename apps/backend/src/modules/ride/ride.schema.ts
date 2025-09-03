a95fd2723f7aac240f70866fb8316550d37d3aec
coder7475 <robiulhossain7475@gmail.com>
2025-09-03 19:58:46 +0600

    feat: update ride request handling to include payment gateway and calculate final fare

--- apps/backend/src/modules/ride/ride.schema.ts
+++ apps/backend/src/modules/ride/ride.schema.ts
@@ -1,20 +1,21 @@
-import { z } from "zod";
-import { RideStatus } from "@/types/types";
+import { PaymentGateway, RideStatus } from "@/types/types";
 import {
-	LocationSchema,
-	rideStatusTimestampsSchema,
+  LocationSchema,
+  rideStatusTimestampsSchema,
 } from "@/types/zod/schemas";
+import { z } from "zod";
 
 export const CreateRideSchema = z.object({
-	pickupLocation: LocationSchema,
-	destinationLocation: LocationSchema,
-	fareEstimated: z.number(),
-	fareFinal: z.number().optional(),
-	timestamps: rideStatusTimestampsSchema.optional(),
+  pickupLocation: LocationSchema,
+  destinationLocation: LocationSchema,
+  fareEstimated: z.number(),
+  fareFinal: z.number().optional(),
+  paymentGateway: z.nativeEnum(PaymentGateway),
+  timestamps: rideStatusTimestampsSchema.optional(),
 });
 export const UpdateRideSchema = CreateRideSchema.partial().extend({
-	driverId: z.string().optional(),
-	rideStatus: z.nativeEnum(RideStatus).optional(),
+  driverId: z.string().optional(),
+  rideStatus: z.nativeEnum(RideStatus).optional(),
 });
 
 export type CreateRideZodType = z.infer<typeof CreateRideSchema>;
