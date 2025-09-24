7894ca47e5743da0b35d666633bab60070d3276f
coder7475 <robiulhossain7475@gmail.com>
2025-09-24 19:34:35 +0600

    refactor: add indexing to riderId and driverId fields in RideSchema for improved query performance

--- apps/backend/src/modules/ride/ride.model.ts
+++ apps/backend/src/modules/ride/ride.model.ts
@@ -1,29 +1,35 @@
-import { type Document, model, Schema } from "mongoose";
 import { LocationSchema } from "@/types/mongoose/location.schema";
 import { RideTimestampsSchema } from "@/types/mongoose/timestamps.schema";
 import { RideStatus } from "@/types/types";
+import { model, Schema, type Document } from "mongoose";
 
 import type { IRide } from "./ride.interface";
 
 export interface IRideDocument extends Document, IRide {}
 
 const RideSchemaMongoose = new Schema<IRideDocument>(
-	{
-		riderId: { type: String, required: true, ref: "User" },
-		driverId: { type: String, required: false, ref: "Driver", default: null },
-		rideStatus: {
-			type: String,
-			enum: Object.values(RideStatus),
-			default: RideStatus.REQUESTED,
-		},
-		pickupLocation: LocationSchema,
-		destinationLocation: LocationSchema,
-		transactionId: { type: String, required: true, ref: "Transaction" },
-		fareEstimated: { type: Number, required: true },
-		fareFinal: { type: Number, required: true, default: 0 },
-		timestamps: { type: RideTimestampsSchema, default: {} },
-	},
-	{ versionKey: false, timestamps: true },
+  {
+    riderId: { type: String, required: true, ref: "User", index: true },
+    driverId: {
+      type: String,
+      required: false,
+      ref: "Driver",
+      default: null,
+      index: true,
+    },
+    rideStatus: {
+      type: String,
+      enum: Object.values(RideStatus),
+      default: RideStatus.REQUESTED,
+    },
+    pickupLocation: LocationSchema,
+    destinationLocation: LocationSchema,
+    transactionId: { type: String, required: true, ref: "Transaction" },
+    fareEstimated: { type: Number, required: true },
+    fareFinal: { type: Number, required: true, default: 0 },
+    timestamps: { type: RideTimestampsSchema, default: {} },
+  },
+  { versionKey: false, timestamps: true },
 );
 
 export const RideModel = model<IRideDocument>("Ride", RideSchemaMongoose);
